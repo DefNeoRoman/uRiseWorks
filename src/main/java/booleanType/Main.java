cb6ddc45a125b79bb07d15188c6f3a1244d6d3b8
DefNeoRoman <d89056411060@yandex.ru>
2017-05-17 07:37:43 +0300

    booleanType

---
+++ src/main/java/booleanType/Main.java
@@ -0,0 +1,44 @@
+package booleanType;
+
+public class Main {
+    public static void main(String[] args) {
+        int ageMisha = 700;
+        int ageVasya = 100;
+        int ageVadim = 150;
+        int old;
+        int mid;
+        int young;
+      if (ageMisha > ageVasya && ageMisha > ageVadim){
+           old = ageMisha;
+          if (ageVasya>ageVadim){
+              mid = ageVasya;
+              young = ageVadim;
+          }else {
+              mid = ageVadim;
+              young = ageVasya;
+          }
+
+      }else if(ageVasya > ageMisha && ageVasya > ageVadim){
+           old = ageVasya;
+          if (ageMisha > ageVadim){
+              mid = ageMisha;
+              young = ageVadim;
+          }else {
+              mid = ageVadim;
+              young = ageMisha;
+          }
+    }else {
+        old = ageVadim;
+          if (ageMisha > ageVasya){
+              mid = ageMisha;
+              young = ageVasya;
+          }else {
+              mid = ageVasya;
+              young = ageMisha;
+          }
+    }
+        System.out.println("oldestMan - " + old);
+        System.out.println("middle age Man - " + mid);
+        System.out.println("most young Man - " + young);
+    }
+}
