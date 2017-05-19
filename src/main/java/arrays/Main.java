ea4ff9d96ee8894d7a8b6d4daeb3ac36505d8e2d
DefNeoRoman <d89056411060@yandex.ru>
2017-05-19 10:44:52 +0300

    arrays Ready

--- src/main/java/arrays/Main.java
+++ src/main/java/arrays/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         String[]letters = {"a","b","c","d","e","f","g","h"};
         int [] numbers = {1,2,3,4,5,6,7,8};
-        for (int i = 0; i < letters.length; i++){
-            for(int j = 0; j < numbers.length; j++){
+        for (int i = numbers.length-1; i>=0; i--){
+            for(int j = 0; j < letters.length; j++){
                 System.out.print(letters[j] + numbers[i] + "|");
             }
             System.out.println();
