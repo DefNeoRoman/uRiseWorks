d4ed0c5f6fafc8aca4fe3f05b8bdad9e53e5f83c
DefNeoRoman <mistarg852455>
2017-05-31 16:17:15 +0300

    binarySortedStorageReady

--- src/main/java/binarySortedStorage/Build.java
+++ src/main/java/binarySortedStorage/Build.java
@@ -1,31 +1,26 @@
 package binarySortedStorage;
-
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Build {
-
     private List<Floor> floors;
+    private int countFloorInBuild = 1;
     private int numCurrentFloor = 0;
     private int currentCountRooms = 1;
     private Floor curFloor = new Floor(currentCountRooms);
-
     public List<Floor> getFloors() {
         return floors;
     }
-
     public Build() {
         floors = new ArrayList<>();
     }
-    public void addFloor(Floor floor){
+    public void addFloor(Floor floor) {
         floors.add(floor);
     }
-    public void whatAppartmentsExist(){
+    public void whatAppartmentsExist() {
         int count = 1;
-        for (Floor f: floors){
-            for (Room r:f.getRooms()){
+        for (Floor f : floors) {
+            for (Room r : f.getRooms()) {
                 System.out.print(r.toString());
             }
             System.out.println();
@@ -34,22 +29,21 @@ public class Build {
             count++;
         }
     }
-    public void addRoom(Room room){
-
-
+    public void addRoom(Room room) {
         if (numCurrentFloor == currentCountRooms) {
             numCurrentFloor = 0;
             currentCountRooms++;
-            curFloor = new Floor(currentCountRooms);
+            curFloor = new Floor(currentCountRooms, countFloorInBuild);
+            countFloorInBuild++;
         }
         if (curFloor.getSize() < currentCountRooms) {
             curFloor.add(room);
         }
         if (curFloor.getSize() == currentCountRooms) {
             addFloor(curFloor);
-            curFloor = new Floor(currentCountRooms);
+            curFloor = new Floor(currentCountRooms, countFloorInBuild);
+            countFloorInBuild++;
             numCurrentFloor++;
-
         }
-   }
+    }
 }
